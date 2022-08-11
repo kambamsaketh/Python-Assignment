92ff88cf698f9f8519e6b826db9052995b5b7589
kambamsaketh <67040512+kambamsaketh@users.noreply.github.com>
2022-08-11 18:32:01 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,170 @@
+
+-- banks table data
+
+insert into banks(bic, bankname) values("ABBLINBBXXX","AB BANK LIMITED");
+insert into banks(bic, bankname) values("ACBLINBBXXX","ABHYUDAYA CO-OPERATIVE BANK LTD.");
+insert into banks(bic, bankname) values("ABNAINBBAHM","ABN AMRO BANK N.V.");
+insert into banks(bic, bankname) values("ADCBINBBXXX","ABU DHABI COMMERCIAL BANK");
+insert into banks(bic, bankname) values("ALLAINBBXXX","ALLAHABAD BANK");
+insert into banks(bic, bankname) values("ANDBINBBXXX","ANDHRA BANK");
+insert into banks(bic, bankname) values("ADIAINBBXXX","ANTWERP DIAMOND BANK N.V.");
+insert into banks(bic, bankname) values("AXISINBBXXX","AXIS BANK LIMITED");
+insert into banks(bic, bankname) values("BOFAIN4XMAA","BANK OF AMERICA, N.A. CHENNAI");
+insert into banks(bic, bankname) values("BOFAIN4XCCU","BANK OF AMERICA, N.A. KOLKATA");
+insert into banks(bic, bankname) values("BBKUINBBXXX","BANK OF BAHRAIN AND KUWAIT B.S.C.");
+insert into banks(bic, bankname) values("BARBINBBXXX","BANK OF BARODA");
+insert into banks(bic, bankname) values("BCEYIN5MXXX","BANK OF CEYLON");
+insert into banks(bic, bankname) values("BKIDINBBXXX","BANK OF INDIA");
+insert into banks(bic, bankname) values("MAHBINBBAHM","BANK OF MAHARASHTRA");
+insert into banks(bic, bankname) values("NOSCINBBBLR","BANK OF NOVA SCOTIA, THE");
+insert into banks(bic, bankname) values("BOTKIN5MXXX","BANK OF TOKYO-MITSUBISHI UFJ, LTD., THE");
+insert into banks(bic, bankname) values("BARCINBBMUM","BARCLAYS BANK PLC");
+insert into banks(bic, bankname) values("BNPAINBBAHM","BNP PARIBAS INDIA");
+insert into banks(bic, bankname) values("CNRBINBBXXX","CANARA BANK");
+insert into banks(bic, bankname) values("CSYBIN55XXX","CATHOLIC SYRIAN BANK LTD");
+insert into banks(bic, bankname) values("CBININBBLAL","CENTRAL BANK OF INDIA");
+insert into banks(bic, bankname) values("CTCBINDDXXX","CHINATRUST COMMERCIAL BANK");
+insert into banks(bic, bankname) values("CITIINBXBLR","CITIBANK N.A.");
+insert into banks(bic, bankname) values("CIUBIN5MXXX","CITY UNION BANK LIMITED");
+insert into banks(bic, bankname) values("CCILINBBXXX","CLEARING CORPORATION OF INDIA LTD, THE");
+insert into banks(bic, bankname) values("CORPINBBXXX","CORPORATION BANK");
+insert into banks(bic, bankname) values("DBSSINBBIBD","DBS BANK LTD, MUMBAI BRANCH");
+insert into banks(bic, bankname) values("BKDNINBBXXX","DENA BANK");
+insert into banks(bic, bankname) values("DEUTINBBBGL","DEUTSCHE BANK AG");
+insert into banks(bic, bankname) values("DCBLINBB036","DEVELOPMENT CREDIT BANK LTD.");
+insert into banks(bic, bankname) values("DLXBINBBKPM","DHANALAKSHMI BANK LTD, THE");
+insert into banks(bic, bankname) values("EIBIINBBXXX","EXPORT-IMPORT BANK OF INDIA");
+insert into banks(bic, bankname) values("FDRLINBBXXX","FEDERAL BANK LIMITED, THE");
+insert into banks(bic, bankname) values("FIRNINBBXXX","FIRSTRAND BANK LIMITED");
+insert into banks(bic, bankname) values("HDFCINBBAHM","HDFC BANK LIMITED");
+insert into banks(bic, bankname) values("HSBCINBBBGE","HONGKONG AND SHANGHAI BANKING CORPORATION LIMITED, THE");
+insert into banks(bic, bankname) values("ICICINBB024","ICICI BANK LIMITED");
+insert into banks(bic, bankname) values("IBKLINBBXXX","IDBI BANK LIMITED");
+insert into banks(bic, bankname) values("IDIBINBBXXX","INDIAN BANK");
+insert into banks(bic, bankname) values("IOBAINBBXXX","INDIAN OVERSEAS BANK");
+insert into banks(bic, bankname) values("INDBINBBXXX","INDUSIND BANK LIMITED");
+insert into banks(bic, bankname) values("VYSAINBBAHD","ING VYSYA BANK LTD.");
+insert into banks(bic, bankname) values("JAKAINBBAGR","JAMMU AND KASHMIR BANK LTD");
+insert into banks(bic, bankname) values("CHASINBXTRO","JPMORGAN CHASE BANK, N.A.");
+insert into banks(bic, bankname) values("VTBRINDDXXX","JSC VTB BANK, NEW DELHI BRANCH");
+insert into banks(bic, bankname) values("KARBINBBBNG","KARNATAKA BANK LTD.");
+insert into banks(bic, bankname) values("KVBLINBBADI","KARUR VYSYA BANK LTD., THE");
+insert into banks(bic, bankname) values("KKBKINBBCPC","KOTAK MAHINDRA BANK LIMITED");
+insert into banks(bic, bankname) values("KRTHINBBXXX","KRUNG THAI BANK PUBLIC COMPANY LTD");
+insert into banks(bic, bankname) values("LAVBINBB114","LAKSHMI VILAS BANK LIMITED");
+insert into banks(bic, bankname) values("MSCIINBBXXX","MAHARASHTRA STATE CO-OPERATIVE BANK LTD., THE");
+insert into banks(bic, bankname) values("MSHQINBBXXX","MASHREQ BANK");
+insert into banks(bic, bankname) values("MHCBINBBXXX","MIZUHO CORPORATE BANK LTD");
+insert into banks(bic, bankname) values("MSMBINBXXXX","MORGAN STANLEY INDIA COMPANY PVT LTD");
+insert into banks(bic, bankname) values("OIBAINBBXXX","OMAN INTERNATIONAL BANK");
+insert into banks(bic, bankname) values("GTBKINBBXXX","ORIENTAL BANK OF COMMERCE");
+insert into banks(bic, bankname) values("PSIBINBB020","PUNJAB AND SIND BANK");
+insert into banks(bic, bankname) values("PUNBINBBXXX","PUNJAB NATIONAL BANK");
+insert into banks(bic, bankname) values("RBISINBBXXX","RESERVE BANK OF INDIA");
+insert into banks(bic, bankname) values("SRCBINBBAUR","SARASWAT CO-OPERATIVE BANK LTD., THE");
+insert into banks(bic, bankname) values("SBCLINBBXXX","SBI COMMERCIAL AND INTERNATIONAL BANK LTD.");
+insert into banks(bic, bankname) values("SHBKINBBXXX","SHINHAN BANK");
+insert into banks(bic, bankname) values("SIDBINBBCBB","SMALL INDUSTRIES DEVELOPMENT BANK OF INDIA");
+insert into banks(bic, bankname) values("SOGEINBBBLR","SOCIETE GENERALE");
+insert into banks(bic, bankname) values("BSONINCCXXX","SONALI BANK");
+insert into banks(bic, bankname) values("SOININ55179","SOUTH INDIAN BANK LTD., THE");
+insert into banks(bic, bankname) values("SCDIIN51XXX","SPARK CAPITAL ADVISORS (INDIA) PRIVATE LTD");
+insert into banks(bic, bankname) values("SCBLINBBMDS","STANDARD CHARTERED BANK");
+insert into banks(bic, bankname) values("SBBJINBBXXX","STATE BANK OF BIKANER AND JAIPUR");
+insert into banks(bic, bankname) values("SBHYINBBXXX","STATE BANK OF HYDERABAD");
+insert into banks(bic, bankname) values("SBININBBXXX","STATE BANK OF INDIA");
+insert into banks(bic, bankname) values("STININBBXXX","STATE BANK OF INDORE");
+insert into banks(bic, bankname) values("STCBINBXNPT","STATE BANK OF MAURITIUS LIMITED");
+insert into banks(bic, bankname) values("SBMYINBBXXX","STATE BANK OF MYSORE");
+insert into banks(bic, bankname) values("STBPINBBXXX","STATE BANK OF PATIALA");
+insert into banks(bic, bankname) values("SBTRINBBXXX","STATE BANK OF TRAVANCORE");
+insert into banks(bic, bankname) values("SYNBINBBXXX","SYNDICATE BANK");
+insert into banks(bic, bankname) values("TMBLINBBCHE","TAMILNAD MERCANTILE BANK LIMITED");
+insert into banks(bic, bankname) values("BRAJINBBARA","THE BANK OF RAJASTHAN LTD.");
+insert into banks(bic, bankname) values("BCMLINBBXXX","THE BHARAT CO-OPERATIVE BANK (MUMBAI) LTD");
+insert into banks(bic, bankname) values("COSDINBBXXX","THE COSMOS CO-OPERATIVE BANK LTD.");
+insert into banks(bic, bankname) values("KALUINAAXXX","THE KALUPUR COMMERCIAL CO-OPERATIVE BANK LIMITED");
+insert into banks(bic, bankname) values("SVCBINBBBLR","THE SHAMRAO VITHAL CO-OPERATIVE BANK LIMITED");
+insert into banks(bic, bankname) values("TCILINBBXXX","THOMAS COOK (INDIA) LTD.");
+insert into banks(bic, bankname) values("UBSWINBBXXX","UBS AG");
+insert into banks(bic, bankname) values("UCBAINBBXXX","UCO BANK");
+insert into banks(bic, bankname) values("UBININBBXXX","UNION BANK OF INDIA");
+insert into banks(bic, bankname) values("UTBIINBBECB","UNITED BANK OF INDIA");
+insert into banks(bic, bankname) values("VIJBINBBXXX","VIJAYA BANK");
+insert into banks(bic, bankname) values("YESBINBBDEL","YES BANK LIMITED");
+
+
+-- message_code table data -----------
+
+insert into message_code(messagecode, instruction) values("CHQB","beneficiary customer must be paid by cheque only.");
+insert into message_code(messagecode, instruction) values("CORT","Payment is made in settlement for a trade.");
+insert into message_code(messagecode, instruction) values("HOLD","Beneficiary customer or claimant will call upon identification.");
+insert into message_code(messagecode, instruction) values("INTC","Payment between two companies that belongs to the same group.");
+insert into message_code(messagecode, instruction) values("PHOB","Please advise the intermediary institution by phone.");
+insert into message_code(messagecode, instruction) values("PHOI","Please advise the intermediary by phone.");
+insert into message_code(messagecode, instruction) values("PHON","Please advise the account with institution by phone.");
+insert into message_code(messagecode, instruction) values("REPA","Payments has a related e-Payments reference.");
+insert into message_code(messagecode, instruction) values("SDVA","Payment must be executed with same day value to the");
+
+
+-- customers table data ---------------
+
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("83020817828620","A KRISHNA MOHAN",true,"56000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("71319440983198","A M MAYANNA",false,"224000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("69652133523248","HDFC BANK -(CHENNAI)",true,"223997","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("78195245302715","A RAJESH KUMAR",true,"35000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("47468155457802","A S VIJAY KUMAR",false,"56000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("45766734966995","ABHISHEK BIYANI",false,"224000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("87139550565094","ABHITHA",false,"223997","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("39145971759304","ABRAHAM VARGHESE",false,"5200","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("83411975607693","ABY GEORGE",false,"15400","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("45002608912874","HDFC BANK -(MUMBAI)",false,"56001","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("15510851190541","AJAY SHAMU KUMAR",false,"224004","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("64059555180495","AKSHAYA WEALTH MANAGEMENT (P) LTD",false,"224001","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("42895235807723","HDFC BANK -(DELHI)",false,"1120005","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("34835543999244","ANAND L S",false,"56001","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("76396804083946","ANANTHAKUMAR",false,"224004","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("14783038759432","ANANTHAMURTHY SHAMASUNDARA",true,"100","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("87030077359017","ANIL GUPTA",false,"25600","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("37437137526069","ANIL KUMAR",false,"35800","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("69443662887399","ANIL KUMAR NAGARAJ",true,"56001","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("27216037942722","HDFC BANK -(GOA)",true,"224004","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("77236671175938","ANIL V GORUR",true,"224001","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("79594243111085","ANITA B MEHTA",true,"-25000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("46957845854316","ANITHA B ASHWATH",true,"56001","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("61331727051606","ANJAN KUMAR G",true,"224004","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("64754104823235","ANKITA BANERJEE",false,"224001","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("80505259371358","ANNUKA NIRVAN",false,"76334","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("63615912257030","ANU",false,"76334","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("13645221972859","ANUPAMA N",false,"95000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("17308739433267","AREEFIKBAL T F",false,"65000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("57577094248867","ARJUN PARAMESWARA",false,"508250","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("89479615455884","ARPITHA M S",false,"-350","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("27225043545104","ASADI LINGAREDDY",false,"1556002","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("20050293665889","ASHAJYOTHI M",false,"224008","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("81181212472499","ASHISH MISHRA",false,"224005","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("71928505646815","ASHOK H R",false,"46000","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("49823469696097","ASHOKA AR",true,"56200","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("98196009547795","AVISHEK MUKHERJEE",false,"56002","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("79029264231605","AYISHA KAUSAR",false,"224008","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("53853797183656","B A SANTOSH",false,"224005","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("57792220815896","B JAYARAM",false,"1120025","roand no X, colony Y","hyderabad","Individual");
+insert into customers(customerid, accountholdername, overdraftflag, clearbalance, customeraddress, customercity, customertype) values("72153770404022","B PRAKASH BABU",true,"0","roand no X, colony Y","hyderabad","Individual");
+
+
+
+------ currency CODES --------
+
+
+Insert into currency(currencycode,currencyname,conversionrate) VALUES("USD","US DOLLAR",74.21);
+Insert into currency(currencycode,currencyname,conversionrate) VALUES("EUR","EUROS",84);
+Insert into currency(currencycode,currencyname,conversionrate) VALUES("GBP","British Pound",102);
+Insert into currency(currencycode,currencyname,conversionrate) VALUES("JPY","Japanese Yen",0.645);
+
+
+
+--------------Transfer TYPES
+
+Insert into transfertype(transfercode,transferdescription) VALUES("Customer Transfer","Transfer to customer");
+Insert into transfertype(transfercode,transferdescription) VALUES("Bank Transfer","Transfer to BANK");
+Insert into transfertype(transfercode,transferdescription) VALUES("Bank Transfer for Own Account","Transfer between bank branches");
